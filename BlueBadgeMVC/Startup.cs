4584fe5045727d0c46a90ee8a6e130de1b24cbf3
zringis <zringis@gmail.com>
2019-05-06 11:08:08 -0400

    Add project files.

---
+++ BlueBadgeMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlueBadgeMVC.Startup))]
+namespace BlueBadgeMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
