4a8c9d96842bb24ec722df24a26afab02ae6d55d
zringis <zringis@gmail.com>
2019-05-07 10:56:04 -0400

    Customer Services Added

---
+++ BlueBadge.Services/CustomerService.cs
@@ -0,0 +1,65 @@
+﻿using BlueBadge.Data;
+using BlueBadge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Services
+{
+    public class CustomerService
+    {
+        private readonly Guid _userId;
+
+        public CustomerService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCustomer(CustomerCreate model)
+        {
+            var entity =
+                new Customer()
+                {
+                    OwnerId = _userId,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    CompanyName = model.CompanyName,
+                    Location = model.Location,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Customers.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CustomerListItem> GetCustomers()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Customers
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new CustomerListItem
+                                {
+                                    CustomerId = e.CustomerId,
+                                    FirstName = e.FirstName,
+                                    LastName = e.LastName,
+                                    CompanyName = e.CompanyName,
+                                    Location = e.Location,
+                                    CreatedUTC = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
