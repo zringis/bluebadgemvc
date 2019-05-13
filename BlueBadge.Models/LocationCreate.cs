2778e5942c6b1430c938584e1627601c6c56e222
zringis <zringis@gmail.com>
2019-05-13 13:33:00 -0400

    location create methods

--- BlueBadge.Models/LocationCreate.cs
+++ BlueBadge.Models/LocationCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace BlueBadge.Models
 {
     public class LocationCreate
     {
+        [Required]
+        [Display(Name = "State")]
+        public string LocationState { get; set; }
+        [Required]
+        [Display(Name = "City")]
+        public string LocationCity { get; set; }
+        [Required]
+        [Display(Name = "Address")]
+        public string LocationAddress { get; set; }
     }
 }
--- BlueBadgeMVC/Controllers/LocationController.cs
+++ BlueBadgeMVC/Controllers/LocationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlueBadge.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,23 @@ namespace BlueBadgeMVC.Controllers
         // GET: Location
         public ActionResult Index()
         {
+            var model = new LocationListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(LocationCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
